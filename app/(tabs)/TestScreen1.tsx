2b416b1541df532b03c7c4965e73b553c3e5e67d
najongjine <najongjin3@hotmail.com>
2026-03-26 10:42:11 +0900

    .

--- app/(tabs)/TestScreen1.tsx
+++ app/(tabs)/TestScreen1.tsx
@@ -1,11 +1,31 @@
-import { ScrollView, StyleSheet, Text } from "react-native";
+import {
+  Button,
+  GestureResponderEvent,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TestScreen1() {
+  let a = 0;
+
+  function onPressLearnMore(event: GestureResponderEvent): void {
+    a++;
+    console.log(`a:${a}`);
+  }
+
   return (
     <SafeAreaView>
       <ScrollView>
         <Text>TestScreen1</Text>
+        <Text>a: {a}</Text>
+        <Button
+          onPress={onPressLearnMore}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </ScrollView>
     </SafeAreaView>
   );
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,55 +6,6 @@ export default function HomeScreen() {
     <SafeAreaView>
       <ScrollView>
         <Text>홈화면 이에요</Text>
-        <Text>홈화면 이에요2</Text>
-        <Text>홈화면 이에요3</Text>
-        <Text>홈화면 이에요4</Text>
-        <Text>홈화면 이에요5</Text>
-        <Text>홈화면 이에요6</Text>
-        <Text>홈화면 이에요7</Text>
-        <Text>홈화면 이에요8</Text>
-        <Text>홈화면 이에요9</Text>
-        <Text>홈화면 이에요10</Text>
-        <Text>홈화면 이에요11</Text>
-        <Text>홈화면 이에요12</Text>
-        <Text>홈화면 이에요13</Text>
-        <Text>홈화면 이에요14</Text>
-        <Text>홈화면 이에요15</Text>
-        <Text>홈화면 이에요16</Text>
-        <Text>홈화면 이에요17</Text>
-        <Text>홈화면 이에요18</Text>
-        <Text>홈화면 이에요19</Text>
-        <Text>홈화면 이에요20</Text>
-        <Text>홈화면 이에요21</Text>
-        <Text>홈화면 이에요22</Text>
-        <Text>홈화면 이에요23</Text>
-        <Text>홈화면 이에요24</Text>
-        <Text>홈화면 이에요25</Text>
-        <Text>홈화면 이에요26</Text>
-        <Text>홈화면 이에요27</Text>
-        <Text>홈화면 이에요28</Text>
-        <Text>홈화면 이에요29</Text>
-        <Text>홈화면 이에요30</Text>
-        <Text>홈화면 이에요31</Text>
-        <Text>홈화면 이에요32</Text>
-        <Text>홈화면 이에요33</Text>
-        <Text>홈화면 이에요34</Text>
-        <Text>홈화면 이에요35</Text>
-        <Text>홈화면 이에요36</Text>
-        <Text>홈화면 이에요37</Text>
-        <Text>홈화면 이에요38</Text>
-        <Text>홈화면 이에요39</Text>
-        <Text>홈화면 이에요40</Text>
-        <Text>홈화면 이에요41</Text>
-        <Text>홈화면 이에요42</Text>
-        <Text>홈화면 이에요43</Text>
-        <Text>홈화면 이에요44</Text>
-        <Text>홈화면 이에요45</Text>
-        <Text>홈화면 이에요46</Text>
-        <Text>홈화면 이에요47</Text>
-        <Text>홈화면 이에요48</Text>
-        <Text>홈화면 이에요49</Text>
-        <Text>홈화면 이에요50</Text>
       </ScrollView>
     </SafeAreaView>
   );
